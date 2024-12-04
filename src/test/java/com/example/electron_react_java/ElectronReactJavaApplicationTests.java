fde6a1d4e21d5282d0b36a72150f8dd9659cfc1f
MarianoDev1601 <mariano.alexei.moran@gmail.com>
2024-12-03 22:50:20 -0400

    first commit

---
+++ src/test/java/com/example/electron_react_java/ElectronReactJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electron_react_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronReactJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
