dc1f60509f2769ede86ce612ca45e170c1b46c6a
MarianoDev1601 <mariano.alexei.moran@gmail.com>
2024-12-04 19:02:27 -0400

    added cors config allowing all origins

--- src/main/java/com/example/electron_react_java/api/TestController.java
+++ src/main/java/com/example/electron_react_java/api/TestController.java
@@ -2,6 +2,7 @@ package com.example.electron_react_java.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.electron_react_java.services.TestService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class TestController {
     private final TestService testService;
